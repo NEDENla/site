a4c36469ad32360ca8406699872650145a1a9ab8
NEDENla <bayraklimustafaemin936@gmail.com>
2024-10-04 18:50:09 +0300

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-// script.js
-document.getElementById("exploreButton").addEventListener("click", function() {
-    document.getElementById("mainContent").scrollIntoView({ behavior: 'smooth' });
-});
