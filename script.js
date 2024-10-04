7fb89490355a9eb20101b336883ee438068b40b0
NEDENla <bayraklimustafaemin936@gmail.com>
2024-10-04 18:17:45 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// script.js
+document.getElementById("exploreButton").addEventListener("click", function() {
+    document.getElementById("mainContent").scrollIntoView({ behavior: 'smooth' });
+});
